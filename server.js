9ed7bf07bc8194b220f40e8cf3e7271e34e2a54f
Cole Simpson <cole22simpson@gmail.com>
2022-04-29 21:35:49 -0700

    quick save

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const { rmSync } = require('fs');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ entended: true }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'cole22simpson',
+        password: '#49ersRulee12345',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
